1cd06558a9947cc0a569271d671c3b0161fd71e1
Ric Li <ricardomlee@gmail.com>
2022-04-09 23:11:53 +0800

    click to show hex (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt::{self, Write};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -16,6 +17,7 @@ pub struct M128i {
 #[wasm_bindgen]
 impl M128i {
     pub fn new() -> M128i {
+        utils::set_panic_hook();
         let mut epi8 = [0 as u8; 16];
         for i in 0..16 {
             epi8[i] += i as u8;
@@ -28,25 +30,37 @@ impl M128i {
             self.epi8[i] += 1;
         }
     }
+    pub fn minus_one(&mut self) {
+        for i in 0..16 {
+            self.epi8[i] -= 1;
+        }
+    }
 
     pub fn render(&self) -> String {
         self.to_string()
     }
-}
 
-use std::fmt;
+    pub fn print_hex(&self) -> String {
+        let mut s = String::new();
+        write!(s, "0x").unwrap_throw();
+        for i in (0..16).rev() {
+            write!(s, "{:02x}", self.epi8[i]).unwrap_throw();
+        }
+        s
+    }
+}
 
 impl fmt::Display for M128i {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for i in 0..16 {
-            let epi = self.epi8[15 - i];
-            for j in 0..8 {
-                let bit = (epi >> (7 - j)) & 0x1;
+        for i in (0..16).rev() {
+            let epi = self.epi8[i];
+            for j in (0..8).rev() {
+                let bit = (epi >> j) & 0x1;
                 let symbol = if bit == 0x0 { '◻' } else { '◼' };
                 write!(f, "{}", symbol)?;
             }
 
-            if (i + 1) % 4 == 0 {
+            if i % 4 == 0 {
                 write!(f, "\n")?;
             } else {
                 write!(f, " ")?;
--- www/index.js
+++ www/index.js
@@ -3,10 +3,23 @@ import {M128i} from "visual-intrinsics";
 const pre = document.getElementById("vi-canvas");
 const m128i = M128i.new();
 document.getElementById("myBtn").onclick = function() {myClick()};
+document.getElementById("minusBtn").onclick = function() { minusClick() };
+
+pre.addEventListener('mousedown', e => {
+  pre.textContent = m128i.print_hex();
+});
+
+pre.addEventListener('mouseup', e => {
+  pre.textContent = m128i.render();
+});
 
 function myClick() {
-  pre.textContent = "";
   m128i.add_one();
+  pre.textContent = m128i.render();
+}
+
+function minusClick() {
+  m128i.minus_one();
   pre.textContent = m128i.render();
 }
 
